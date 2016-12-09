5c65524424c301a3c5abbfca5616597144ab8149
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-09 12:29:25 +0000

    Remove redundant CSS selectors

--- lib/member_page.rb
+++ lib/member_page.rb
@@ -2,12 +2,12 @@ require 'scraped'
 
 class MemberPage < Scraped::HTML
   field :name do
-    noko.xpath('//div[@class="col-sm-12 col-md-2 distrito"]/following-sibling::div[@class="col-sm-12 col-md-4"]/h1/text()')
+    noko.xpath('//div[contains(@class, "distrito")]/following-sibling::div/h1/text()')
         .text
         .tidy
   end
   field :phone do
-    noko.xpath('//div[@class="col-sm-12 col-md-2 verticalPad" and contains(.,"Teléfono")]/text()')
+    noko.xpath('//div[contains(@class, "verticalPad") and contains(.,"Teléfono")]/text()')
         .text
         .split(':')
         .last
@@ -15,7 +15,7 @@ class MemberPage < Scraped::HTML
   end
 
   field :email do
-    noko.at_xpath('//div[@class="col-sm-12 col-md-2 verticalPad"]/a[@href[contains(.,"contacto")]]/text()')
+    noko.at_xpath('//div[contains(@class, "verticalPad")]/a[@href[contains(.,"contacto")]]/text()')
         .text
         .tidy
   end
